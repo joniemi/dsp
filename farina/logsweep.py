79a0ab6c0885ac4d56457ab305fab71c7934ced2
Jussi Nieminen <j.n.8.9@hotmail.com>
2023-04-21 22:41:25 +0200

    extend Farina method

--- farina/logsweep.py
+++ farina/logsweep.py
@@ -1,29 +1,49 @@
 """ Functions that implement impulse response measurements using
 A. Farina's logarithmic sweep method.
 """
+import itertools as it
 import numpy as np
 from scipy import signal
 
 
 def generate(f0, f1, fs, duration) -> np.ndarray:
     num_samples = int(duration * fs)
-    t = np.linspace(0, duration, num_samples)
-    y = signal.chirp(t, f0, duration, f1, method="logarithmic", phi=270)
+    t = np.linspace(0, 1, num_samples)
+    y = signal.chirp(t, f0=f0, t1=1, f1=f1, method="logarithmic", phi=270)
 
     return y
 
 
-def apply_window(x: np.ndarray, ramp_length):
+def apply_window(x: np.ndarray, ramp_length: int) -> None:
     win = signal.windows.hann(ramp_length * 2)
     x[:ramp_length] *= win[:ramp_length]
     x[-ramp_length:] *= win[-ramp_length:]
 
 
-def inverse_filter(f0, f1, num_samples):
-    pass
+def inverse_filter(x: np.ndarray, f0: float, f1: float) -> np.ndarray:
+    num_samples = x.shape[-1]
+    start = 1.0
+    end = 10 ** (-6 * np.log2(f1 / f0) / 20)
+    envelope = np.logspace(np.log(start), np.log(end), num_samples, base=np.e)
+    inv_filter = np.flip(x) * envelope
+    return inv_filter
 
 
-def magnitude_response(x, inv_filter):
-    # convolve with inverse filter
-    # FFT and dB
-    pass
+def impulse_response(sweep_wet: np.ndarray, inv_filter: np.ndarray):
+    ir = signal.fftconvolve(sweep_wet, inv_filter)
+    return ir
+
+
+def thd_impulse_responses(
+        ir: np.ndarray, f0: float, f1: float, num_harmonics: int, ramp_len: int = 256):
+    length = ir.shape[-1] // 2
+    deltas = [np.log(n) / np.log(f1 / f0) * length for n in range(2, num_harmonics + 2)]
+    positions = [length] + [int(length - d) for d in deltas]
+    thds = [ir[x0: x1] for x1, x0 in it.pairwise(positions)]
+
+    win = signal.windows.hann(ramp_len * 2)
+
+    for i in range(len(thds)):
+        thds[i][-ramp_len:] *= win[-ramp_len:]
+
+    return thds
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import matplotlib.pyplot as plt
+import numpy as np
+
 from common import plots
 from farina import logsweep
 
@@ -13,6 +15,21 @@ def main():
     plt.figure()
     plt.plot(x)
     plots.plot_magnitude_and_phase(x, fs)
+
+    # simulate distortion
+    y = x * 1.05
+    y = np.clip(y, -1.0, 1.0)
+
+    inv_filter = logsweep.inverse_filter(x, f0, f1)
+    ir = logsweep.impulse_response(y, inv_filter)
+    thd_responses = logsweep.thd_impulse_responses(ir, f0, f1, 4)
+
+    plt.figure()
+
+    for thd_resp in thd_responses:
+        plt.semilogx(thd_resp)
+
+    plt.show()
 
     return 0
 
